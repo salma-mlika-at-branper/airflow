67766d0345ce891049eee69bead4dc9aab329a7e
salma.mlika <salma.mlika@branper.com>
2026-04-27 17:52:09 +0000

    testv1

--- dags/arabic.py
+++ dags/arabic.py
@@ -29,7 +29,7 @@ def load_data(**kwargs):
 # STEP 2: Load pretrained model (store only model name)
 # ----------------------------
 def load_model(**kwargs):
-    model_name = "/opt/airflow/models/pretrained_model"
+    model_name = "/opt/airflow/models/twitter_sentiment_finetuned"
     kwargs["ti"].xcom_push(key="model_name", value=model_name)
 
 # ----------------------------
