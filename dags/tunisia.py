8d4720bc0fe3a9a32f2e89df365efddf7b9f261a
salma.mlika <salma.mlika@branper.com>
2026-05-05 16:04:48 +0000

    test

--- dags/tunisia.py
+++ dags/tunisia.py
@@ -34,7 +34,7 @@ def load_data(**kwargs):
 # STEP 2: Load pretrained model (store only model name)
 
 def load_model(**kwargs):
-    model_name ="/opt/airflow/models/additive_finetuned_model"
+    model_name ="/opt/airflow/models/partial_finetuned_model"
 
     kwargs["ti"].xcom_push(key="model_name", value=model_name)
 
