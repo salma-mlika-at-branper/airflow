2c0659013b58c535e50a2a4e02a12e5cad6f0848
salma.mlika <salma.mlika@branper.com>
2026-05-10 13:32:19 +0000

    fix

--- sentiment-ui/model_loader.py
+++ sentiment-ui/model_loader.py
@@ -25,7 +25,7 @@ OLLAMA_HOSTS = [
     "http://host.docker.internal:11434",
     "http://172.17.0.1:11434",
 ]
-OLLAMA_MODEL = "aya:8b:latest"
+OLLAMA_MODEL = "aya:8b"
 
 
 def _ollama_post(path: str, payload: dict) -> dict:
